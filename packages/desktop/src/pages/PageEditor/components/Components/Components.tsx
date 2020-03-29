874e5c61d3b06aaf99ac955f86c1d0629b0f5403
Lionel Bertrand <lionel.bertrand@ymail.com>
2020-03-29 15:15:30 +0200

    share

--- packages/desktop/src/pages/PageEditor/components/Components/Components.tsx
+++ packages/desktop/src/pages/PageEditor/components/Components/Components.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { Card } from 'antd';
+import { ResizableBox } from 'react-resizable';
+import styled from 'styled-components';
 
 import { componentContext } from '../../context/component';
 import { ListComponent } from './components/ListComponent';
 import { EditComponent } from './components/EditComponent';
 
+const Rezise = styled.div`
+  background-color: black;
+`;
 const contentList = {
   edit: <EditComponent />,
   components: <ListComponent />
@@ -40,16 +45,25 @@ const Components = () => {
   if (!open) return null;
 
   return (
-    <Card
-      bordered={false}
-      tabList={tabListNoTitle}
-      activeTabKey={tabIndex}
-      onTabChange={toggleTab}
-      style={{ display: 'flex', flexDirection: 'column' }}
-      bodyStyle={{ flex: 1, overflow: 'auto' }}
+    <ResizableBox
+      className="resizeBox"
+      handle={<Rezise />}
+      width={200}
+      height={200}
+      minConstraints={[100, 100]}
+      maxConstraints={[300, 300]}
     >
-      {contentList[tabIndex]}
-    </Card>
+      <Card
+        bordered={false}
+        tabList={tabListNoTitle}
+        activeTabKey={tabIndex}
+        onTabChange={toggleTab}
+        style={{ display: 'flex', flexDirection: 'column' }}
+        bodyStyle={{ flex: 1, overflow: 'auto' }}
+      >
+        {contentList[tabIndex]}
+      </Card>
+    </ResizableBox>
   );
 };
 
